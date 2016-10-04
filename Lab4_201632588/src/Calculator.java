95472444fe64a8ed2029d1f10332887576338e4a
TaeYoon <replayfish@ajou.ac.kr>
2016-10-04 10:59:08 +0900

    i've changed again

--- Lab4_201632588/src/Calculator.java
+++ Lab4_201632588/src/Calculator.java
@@ -8,24 +8,18 @@
  */
 public class Calculator {
 	public double add(double x,double y){
-		return 0;
+		return a+b;
 	}
 	public double substract(double x,double y){
 		return 0;
 	}
 	public double multiply(double x,double y){
-		return 0;
+		return a*b;
 	}
 	public double divide(double x,double y){
 		return 0;
 	}
 	
-	public int add(int a, int b){
-		return a+b;
-	}
-	public int multiply(int a, int b){
-		return a*b;
-	}
 	public String toString(){
 		System.out.println("Leader name : sonicvi");
 		System.out.println("Leader ID : 201632588");
