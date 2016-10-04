82889cad877c06222b7ffbdf9691fb167885ac0c
loicv <loicv@SONICVI-ROG>
2016-10-04 10:37:37 +0900

    commited

---
+++ Lab4_201632588/src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+	public double add(double x,double y){
+		return 0;
+	}
+	public double substract(double x,double y){
+		return 0;
+	}
+	public double multiply(double x,double y){
+		return 0;
+	}
+	public double divide(double x,double y){
+		return 0;
+	}
+}
