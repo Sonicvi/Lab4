adcb6d1f779507f96796896eb4098c1d6fbca83a
dongzoor <dongjoo92@gmail.com>
2016-10-04 11:09:52 +0900

    dd

--- Lab4_201632588/src/Calculator.java
+++ Lab4_201632588/src/Calculator.java
@@ -1,6 +1,10 @@
 /*
  * Lab assignment 4
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Sonicvi/Lab4.git
+ * Team leader : Loc Villaum 201632588
+=======
  * Team leader : Lo�c Villaum� 201632588
+>>>>>>> f2ad2f5 hi
  * Programmer A :
  * Programmer B : 
  * 
@@ -8,21 +12,23 @@
  */
 public class Calculator {
 	public double add(double x,double y){
-		return 0;
+		return x+y;
 	}
 	public double substract(double x,double y){
 		return x-y;
 	}
 	public double multiply(double x,double y){
-		return 0;
+		return x*y;
 	}
 	public double divide(double x,double y){
 		
 		if(x == 0)
 			return 0;
 		
-		else if(y == 0)
-			System.out.println("Can't divide by zero");
+		else if(y == 0){
+			 System.out.println("Can't divide by zero");
+			return 0;
+		}
 		else
 			return x/y;
 	}
@@ -31,4 +37,14 @@ public class Calculator {
 		return (x - (x/y)*y);
 		
 	}
+	
+	public void toString(){
+		System.out.println("Leader name : sonicvi");
+		System.out.println("Leader ID : 201632588");
+		System.out.println("haeyong, dongjoo");
+		System.out.println("Programmer A's ID : 201120268");
+		System.out.println("Programmer B's ID : 201120287");
+		System.out.println("https://github.com/Sonicvi/Lab4.git");
+	}
 }
+
