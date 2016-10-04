f2ad2f587f6fe6d98f3e6857f10d811b4ababbf4
dongzoor <dongjoo92@gmail.com>
2016-10-04 10:56:55 +0900

    hi

--- Lab4_201632588/src/Calculator.java
+++ Lab4_201632588/src/Calculator.java
@@ -1,6 +1,6 @@
 /*
  * Lab assignment 4
- * Team leader : Loïc Villaumé 201632588
+ * Team leader : Loï¿½c Villaumï¿½ 201632588
  * Programmer A :
  * Programmer B : 
  * 
@@ -11,12 +11,24 @@ public class Calculator {
 		return 0;
 	}
 	public double substract(double x,double y){
-		return 0;
+		return x-y;
 	}
 	public double multiply(double x,double y){
 		return 0;
 	}
 	public double divide(double x,double y){
-		return 0;
+		
+		if(x == 0)
+			return 0;
+		
+		else if(y == 0)
+			System.out.println("Can't divide by zero");
+		else
+			return x/y;
+	}
+	public int remainder(int x,int y){
+		
+		return (x - (x/y)*y);
+		
 	}
 }
