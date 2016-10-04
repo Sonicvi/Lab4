7e24597788b13f71a8b188b2f543bac4df537d3b
loicv <loicv@SONICVI-ROG>
2016-10-04 11:24:05 +0900

    k

--- Lab4_201632588/src/CalculatorTest.java
+++ Lab4_201632588/src/CalculatorTest.java
@@ -2,7 +2,7 @@
 public class CalculatorTest {
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
-		System.out.println("add 3.5 + 9"+calc.add(3.5, 9));
+		System.out.println("add 3.5 + 9: "+calc.add(3.5, 9));
 		System.out.println("substract 3.5 - 9 : "+calc.substract(3.5, 9));
 		System.out.println("multiply 3.5 x 9 : "+calc.multiply(3.5, 9));
 		System.out.println("divide 3.5 / 9 : "+calc.divide(3.5, 9));
